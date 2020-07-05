3b010cf55dd316c98d02543a6c5b845f770b6dfc
sehan <sehandev@gmail.com>
2020-07-05 13:58:12 +0900

    EDIT : starts ajax url

--- web/static/js/start_ajax.js
+++ web/static/js/start_ajax.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     $.ajax({
         url: 'http://api.sehandev.com/starts/' + document.getElementById("user-id").textContent,
-        // url: 'http://api.sehandev.com/starts/' + '5ef5a92b6d3b9f556188a48b',
         type: 'GET',
         dataType: "json",
         cache: true,
