4991c90c8bd3ea2d9c02aae121a0301abd17dc41
sehan <sehandev@gmail.com>
2020-09-03 15:23:46 +0900

    EDIT : views.js jquery 제거

--- web/static/js/views.js
+++ web/static/js/views.js
@@ -1,7 +1,6 @@
-$("#menu-button").click(() => {
-    $("#nav-overlay").toggleClass("hide-menu")
+document.getElementById("menu-button").addEventListener("click", () => {
+    document.getElementById("nav-overlay").classList.toggle("hide-menu")
 })
-
-$("#nav-overlay").click(() => {
-    $("#nav-overlay").toggleClass("hide-menu")
+document.getElementById("nav-overlay").addEventListener("click", () => {
+    document.getElementById("nav-overlay").classList.toggle("hide-menu")
 })
