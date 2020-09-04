7544547f03968ccf0c4f71935f6a35d55592f824
sehan <sehandev@gmail.com>
2020-09-04 19:48:37 +0900

    RM : jquery ajax 대신 xhr 사용

--- web/static/js/start_ajax.js
+++ web/static/js/start_ajax.js
@@ -1,14 +1,22 @@
-$(document).ready(() => {
-    $.ajax({
-        url: "http://api.sehandev.com/starts/" + document.getElementById("user-id").textContent,
-        type: "GET",
-        dataType: "json",
-        cache: true,
-        success: (data) => {
-            data.forEach((start_map) => {
-                console.log(start_map)
-                $("#start-list").append("<li>" + '<a href="/views/' + start_map["txt_id"] + '">' + start_map["txt_title"] + "</a>" + "</li>")
+// load 완료하면 ajax로 id에 해당하는 txt 불러오기
+document.addEventListener("DOMContentLoaded", () => {
+    let user_id = document.getElementById("user-id").textContent
+    var xhr = new XMLHttpRequest()
+    var url = "http://api.sehandev.com/starts/" + user_id
+    xhr.open("GET", url, true)
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var ajax_data = JSON.parse(xhr.responseText)
+            ajax_data.forEach((data) => {
+                let li = document.createElement("li")
+                li.innerHTML = `
+                    <a href="/views/${data["txt_id"]}">
+                        ${data["txt_title"]}
+                    </a>
+                    `
+                document.getElementById("start-list").appendChild(li)
             })
-        },
-    })
+        }
+    }
+    xhr.send()
 })
